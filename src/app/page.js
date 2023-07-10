d9f1108d334b6d89d36c5ebdb899e2bc6065f9db
Majed <majed@Majeds-iMac.local>
2023-07-10 09:33:30 +0400

    edit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,129 @@
-import Image from 'next/image'
+'use client'
+import { useEffect, useState } from 'react';
+
+import React from 'react';
+import moment from 'moment-hijri';
+
+
+
+
+const months = [
+    "محرم",
+    "صفر",
+    "ربيع الأول",
+    "ربيع الثاني",
+    "جمادى الأولى",
+    "جمادى الآخرة",
+    "رجب",
+    "شعبان",
+    "رمضان",
+    "شوال",
+    "ذو القعدة",
+    "ذو الحجة"
+]
+const weekdays = ['الأحد', 'الاثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت'];
+
+
+
+// const moment1 = moment('1410/8/28', 'iYYYY/iM/iD');
+var calendar = [];
+const startDate = moment('1444/1/1', 'iYYYY/iM/iD')
+    .clone()
+    .startOf("iMonth")
+    .subtract(6, "day")
+console.log(startDate);
+const endDate = startDate
+    .clone()
+    .endOf("iMonth")
+const day = startDate.clone().subtract(1, "day");
+while (day.isBefore(endDate, "day")) {
+    console.log("zg")
+    // const date = day.add(1, "day").clone();
+    // const dow = date.day();
+    // const array = [];
+    // array[dow] = date.format.format("DD")
+    // const date  = day.add(1, "day").clone();
+    //     const dow = date.day();
+    // const arr = Array(7)
+    // .fill(0);
+    // arr[dow] = date.format("iDD")
+    // console.log(arr)
+    calendar.push(
+        Array(7)
+            .fill(0)
+            .map((index) => {
+                const date = day.add(1, "day").clone();
+                const dow = date.day();
+                console.log(dow)
+                return date.format("iDD")
+            }
+            )
+    );
+    // Array(7)
+    //     .fill(0).map
+    // for (let i = 0; i < 7; i++) {
+    //     console.log(i)
+    //   } 
+
+}
+console.log(calendar)
+
+
+const getHijriMonth = (date) => {
+    const hijriDate = new Intl.DateTimeFormat('ar-TN-u-ca-islamic', {
+        month: 'long',
+    }).format(date);
+
+    return hijriDate;
+};
+
+const getHijriDay = (data) => {
+    const hijriDay = data.toLocaleString('en-US-u-ca-islamic', {
+        day: 'numeric',
+    });
+
+    return parseInt(hijriDay);
+};
+const index = {
+    "محرم": 4,
+    "صفر": 35,
+    "ربيع الأول": 67,
+    "ربيع الآخر": 99,
+    "جمادي الأولى": 130,
+    "جمادي الآخرة": 162,
+    "رجب": 193,
+    "شعبان": 224,
+    "رمضان": 256,
+    "شوال": 288,
+    "ذو القعدة": 320,
+    "ذو الحجة": 350
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const lastPage = 380;
+    const hijirMonth = getHijriMonth(currentDate);
+    const hijriDay = getHijriDay(currentDate)
+    const [currentPage, setCurrentPage] = useState(index[hijirMonth] + hijriDay);
+
+    const previousPage = () => {
+        setCurrentPage(currentPage - 1);
+    };
+
+    const nextPage = () => {
+        setCurrentPage(currentPage + 1);
+    };
+
+
+
+    return (
+        <div>
+            <img src={`/hijri/pages/${currentPage.toString().padStart(3, 0)}`} />
+            <div className='flex gap-4'>
+                {currentPage < lastPage && <button onClick={nextPage}>التالي</button>}
+                {currentPage > 1 && <button onClick={previousPage}>السابق</button>}
+            </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
 }
